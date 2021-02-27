7e8cc6f5fc280ff6d843019029c2df928a0c764c
Dilan KAYA <dkaya7597@hotmail.com>
2021-02-27 22:12:30 +0300

    27.Şubat

---
+++ Core/Entities/Concrete/OperationClaim.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities.Concrete
+{//tablodaki güvenlik için oluşturduğum tablolar
+    class OperationClaim:IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ Core/Entities/Concrete/UseroperationClaim.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities.Concrete
+{//tablodaki güvenlik için oluşturduğum tablolar
+    class UserOperationClaim:IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+
+    }
+}
